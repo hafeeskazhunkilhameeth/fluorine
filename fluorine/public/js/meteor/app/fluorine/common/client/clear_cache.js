04804f202b74e152538aeb955fa5e4f384563a4a
Luis Fernandes <luisfmfernandes@gmail.com>
2015-07-22 15:47:43 +0100

    fluorine remove public/js files.

--- fluorine/public/js/meteor/app/fluorine/common/client/clear_cache.js
+++
@@ -1,5 +0,0 @@
-(function(){Meteor_clear_cache = function(){
-	Package.reload.Reload._reload();
-}
-
-})();
--- fluorine/public/js/meteor/meteor_url_prefix.js
+++
@@ -1,1 +0,0 @@
-__meteor_runtime_config__.ROOT_URL_PATH_PREFIX = 'http://192.168.1.100:3000';
