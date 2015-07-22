fe206bc168cf635578af235629af2439d8449146
Luis Fernandes <luisfmfernandes@gmail.com>
2015-07-22 18:10:47 +0100

    fluorine remove public/js files.

--- fluorine/hooks.py
+++ fluorine/hooks.py
@@ -12,4 +12,5 @@ app_include_js=[]
 #home_page=["fluorine_home"]
 app_version=["0.0.1"]
 app_email=["luisfmfernandes@gmail.com"]
-
+before_install = "fluorine.utils.install.before_install"
+after_install = "fluorine.utils.install.after_install"
