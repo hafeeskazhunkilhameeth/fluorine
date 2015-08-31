e2ffb756c8e46417ad4df04e2a99996425a68c4d
Luis Fernandes <luisfmfernandes@gmail.com>
2015-08-31 12:34:11 +0100

    Add ste-state init option to start meteor after instalation.

--- fluorine/commands_helpers/meteor.py
+++ fluorine/commands_helpers/meteor.py
@@ -310,8 +310,8 @@ def meteor_run(app, app_path, mongo_custom=False):
 			mongo_url = "MONGO_URL=mongodb://%s:%s/%s " % (host, port, db)
 
 	args = shlex.split("%smeteor --port %s" % (mongo_url, str(PORT.get(app))))
-	click.echo("meteor command {}".format(args))
 	meteor = subprocess.Popen(args, cwd=app_path, shell=False, stdout=subprocess.PIPE)
+	click.echo("meteor command {}".format(args))
 	while True:
 		line = meteor.stdout.readline()
 		if "App running at" in line:
