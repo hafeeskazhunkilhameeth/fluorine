bd2c043087baa3244b3a56d4511ad479acb42332
Luis Fernandes <luisfmfernandes@gmail.com>
2015-09-02 17:06:22 +0100

    Install Meteor packages with versions.

--- fluorine/utils/compile.py
+++ fluorine/utils/compile.py
@@ -8,11 +8,11 @@ import frappe
 @frappe.whitelist(allow_guest=True)
 def meteor_compile():
 	from fluorine.fluorine.doctype.fluorine_reactivity.fluorine_reactivity import prepare_make_meteor_file
-	from fluorine.utils import meteor_config
-	from fluorine.commands_helpers import get_doctype
+	from fluorine.utils import meteor_config, meteor_web_app
+	#from fluorine.commands_helpers import get_doctype
 
 	if meteor_config.get("developer_mode") and not meteor_config.get("stop"):
 		frappe.set_user("Administrator")
-		doc = get_doctype("Fluorine Reactivity", frappe.local.site)
-		prepare_make_meteor_file(doc.fluor_meteor_port, "Reactive Web")
+		#doc = get_doctype("Fluorine Reactivity", frappe.local.site)
+		prepare_make_meteor_file(meteor_web_app)
 		print "compile_meteor called for site {}.".format(frappe.local.site)
