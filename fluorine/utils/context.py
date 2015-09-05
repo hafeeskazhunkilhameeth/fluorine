68a18726806158924ee4ce91c48e44e6666af5a3
Luis Fernandes <luisfmfernandes@gmail.com>
2015-09-05 22:12:07 +0100

    Divide code by modules..

--- fluorine/utils/context.py
+++ fluorine/utils/context.py
@@ -54,7 +54,7 @@ def prepare_context_meteor_file(whatfor):
 	from fluorine.templates.pages.fluorine_home import get_context as fluorine_get_context
 	from fluorine.utils import meteor_desk_app, fluor_get_context as get_context
 
-	#prepare_compile_environment(whatfor)
+
 	if whatfor == meteor_desk_app:
 		frappe.local.path = "desk"
 		return get_context("desk")
