4aad3c3409f605c6a3594ce45bde382ff4a7bd19
Luis Fernandes <luisfmfernandes@gmail.com>
2015-09-05 21:49:43 +0100

    Divide code by modules..

--- fluorine/utils/context.py
+++ fluorine/utils/context.py
@@ -1,6 +1,53 @@
 __author__ = 'luissaguas'
 
-import frappe
+import frappe,os, click
+from fluorine.utils import whatfor_all, meteor_desk_app
+
+
+class MeteorContext(object):
+	def __init__(self, production=True):
+		self.context = frappe._dict({meteor_desk_app:None})
+		frappe.local.making_production = production
+
+	def meteor_init(self, mongo_custom=False):
+		from fluorine.utils.file import get_path_reactivity
+		from fluorine.commands_helpers.meteor import meteor_run
+
+
+		for app in whatfor_all:
+			app_path = os.path.join(get_path_reactivity(), app)
+			program_json_path = os.path.join(app_path, ".meteor", "local", "build", "programs", "web.browser", "program.json")
+			if not os.path.exists(program_json_path) and os.path.exists(os.path.join(app_path, ".meteor")):
+				try:
+					meteor_run(app, app_path, mongo_custom=mongo_custom)
+				except Exception as e:
+					click.echo("You have to start meteor at hand before start meteor. Issue `meteor` in %s. Error: %s" % (app_path, e))
+					return
+
+	def make_context(self):
+		from fluorine.utils import prepare_environment
+		from fluorine.utils.reactivity import start_meteor
+		from fluorine.utils.finals import make_public_folders
+		#from fluorine.command import prepare_make_meteor_file
+
+		make_public_folders()
+		prepare_environment()
+		start_meteor()
+		frappe.local.request = frappe._dict()
+
+		for w in whatfor_all:
+			#prepare_compile_environment(w)
+			ctx = prepare_make_meteor_file( w)
+			if w == meteor_desk_app:
+				self.context[meteor_desk_app] = ctx
+
+	def make_meteor_properties(self):
+		from fluorine.utils.meteor.utils import make_meteor_props
+		from fluorine.utils.spacebars_template import make_includes
+
+		context = self.context.get(meteor_desk_app)
+		make_meteor_props(context, meteor_desk_app, production=True)
+		make_includes(context)
 
 
 def prepare_make_meteor_file(whatfor):
